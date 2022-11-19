de95c32f1a029e14bc03c4410d8c2add96c29173
sadunenko <a.sadunenko@gmail.com>
2022-11-19 09:01:36 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Tanya on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
